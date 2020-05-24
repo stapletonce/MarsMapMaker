adf7a037111464203e9ca8072d62ecad151330a0
Josh Gilley <joshdgilley@gmail.com>
2020-05-24 19:15:11 -0400

    added date formatting function and it works for the cases with delimiters

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -19,15 +19,15 @@ const CardList = (props) => {
     const useOnce = []
     let newKey = -1
     const dateFormatOption = [
-        { title: "Choose Date Format" },
-        { title: "DD/MM/YYYY or DD-MM-YYYY", value: "substring", type: "date" },
-        { title: "MM/DD/YYYY or MM-DD-YYYY", value: "substring", type: "date" },
-        { title: "YYYY/DD/MM or YYYY-DD-MM", value: "substring", type: "date" },
-        { title: "YYYY/MM/DD or YYYY-MM-DD", value: "substring", type: "date" },
+        { title: "" },
+        { title: "DD/MM/YY or DD-MM-YY", value: "substring", type: "date" },
+        { title: "MM/DD/YY or MM-DD-YY", value: "substring", type: "date" },
+        { title: "YY/DD/MM or YY-DD-MM", value: "substring", type: "date" },
+        { title: "YY/MM/DD or YY-MM-DD", value: "substring", type: "date" },
         { title: "MMDDYYYY", value: "substring", type: "date" },
         { title: "DDMMYYYY", value: "substring", type: "date" },
         { title: "YYYYDDMM", value: "substring", type: "date" },
-        { title: "YYYYMMDD", value: "substring", type: "date" },
+        { title: "YYYYMMDD", value: "substring", type: "date" }
     ]
 
     // used to hide 'non-green / non-checked fields in the UI (hides field and checks)
