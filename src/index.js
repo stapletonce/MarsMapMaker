7fa6b21354de5de25dbd317000657fe6b34671f4
Robert <niggebruggere@g.cofc.edu>
2020-05-26 16:49:33 -0400

    adding a log

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 import reducer from './store/reducer';
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-console.log("hello world")
->>>>>>> 5657cf4af2ad7622bd87bb37340ab3375777aefb
-=======
->>>>>>> 56ff9eafce18fcd0ac6438fd21f2c124e482708d
 
+console.log("hello")
 
 ReactDOM.render(
     <Provider store={createStore(reducer)}>
