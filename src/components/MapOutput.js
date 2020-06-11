c584f3461622d8af5f830cf463aa86d9e7f94cff
Robert <niggebruggere@g.cofc.edu>
2020-06-11 12:51:23 -0400

    COMPLETED: mapping object inside of javascript file

--- src/components/MapOutput.js
+++ src/components/MapOutput.js
@@ -10,7 +10,7 @@ class MapOutput extends React.Component {
         super(props);
 
     }
-
+    //this method loops through the array entries in the store multiple times to append to the string based on corresponding SesarTitles selected that
     createMapString() {
         let letMapString = "let map = {\n"
         let lastIndexOfContent = -1
@@ -21,7 +21,9 @@ class MapOutput extends React.Component {
         let size_found = -1
 
         //for formatting need to track the relative last entry of each multivalue and single value and the last entry used
+        
         for (let j = 0; j < this.props.ent.length; j++){
+            //these conditionals track the last occurance of each type of sesarTitle
             if (this.props.ent[j].sesarTitle !==  "" && this.props.ent[j].sesarTitle !== "field_name" && this.props.ent[j].sesarTitle !== "sample_comment" && this.props.ent[j].sesarTitle !== "description" && this.props.ent[j].sesarTitle !== "size")
                 singleLastIndexOfContent = j
             else if (this.props.ent[j].sesarTitle === "field_name")
@@ -34,18 +36,19 @@ class MapOutput extends React.Component {
                 size_found = j
         }
         
+        //this finds the overall last occurance of a value in the array
         const findFinalPosition = [singleLastIndexOfContent, sample_found, description_found, field_found, size_found]
         lastIndexOfContent = Math.max(...findFinalPosition)
         
+        
         let singlesAppendingString = "";
         for (let i = 0; i < this.props.ent.length; i++) {
-            //adding the single mapped values to the string
             if (this.props.ent[i].sesarTitle !== "" &&
             this.props.ent[i].sesarTitle !== "field_name" &&
             this.props.ent[i].sesarTitle !== "sample_comment" &&
             this.props.ent[i].sesarTitle !== "description" &&
             this.props.ent[i].sesarTitle !== "size"){
-                
+  
                 if (i !== singleLastIndexOfContent || i + 1 >= this.props.ent.length)
                     singlesAppendingString += "     " + this.props.ent[i].sesarTitle + ": " + this.props.ent[i].header + ",\n"
                 else if (i === lastIndexOfContent && field_found < 0 && sample_found < 0 && size_found < 0 && description_found < 0)
@@ -55,29 +58,69 @@ class MapOutput extends React.Component {
             }
             
         }
-        alert("field found value is " + field_found)
-        alert(lastIndexOfContent)
-        //add the multiValues to the string
-        //make sure to check if last line
+       
         let multiAppendingString = ""
-        if (field_found >= 0) {
-            console.log("hey im here field found is : "+ field_found)
+        if (field_found > -1) {
+            
             multiAppendingString += "     field_name: ["
             for (let z = 0; z < this.props.ent.length; z++){
                 if (this.props.ent[z].sesarTitle === "field_name"){
-                    if (z === lastIndexOfContent && sample_found < 0 && size_found < 0 && description_found < 0)
-                        multiAppendingString += " \"" + this.props.ent[z].header + "\"]"
-                    else if (z === field_found && sample_found > -1 && size_found > -1 && description_found > -1)
+                    if (z === field_found && (sample_found < 0 || size_found < 0 || description_found < 0))
+                        multiAppendingString += " \"" + this.props.ent[z].header + "\"]\n"
+                    else if (z === field_found && (sample_found > -1 || size_found > -1 || description_found > -1))
                         multiAppendingString += " \"" + this.props.ent[z].header + "\"],\n"
                     else if (z < lastIndexOfContent)
-                        multiAppendingString += " \"" + this.props.ent[z].header + "\","
+                        multiAppendingString += "\"" + this.props.ent[z].header + "\", "
                 }
                 
+
             
-                
             }
         }
-            let appendingString = singlesAppendingString + multiAppendingString + "\n}\n\n"
+        if (sample_found > -1){
+            
+            multiAppendingString += "     sample_comment: ["
+            for (let z = 0; z < this.props.ent.length; z++){
+                if (this.props.ent[z].sesarTitle === "sample_comment"){
+                    if (z === sample_found && (size_found < 0 || description_found < 0))
+                        multiAppendingString += "\"" + this.props.ent[z].header + "\"]\n"
+                    else if (z === sample_found && (size_found > -1 || description_found > -1))
+                        multiAppendingString += " \"" + this.props.ent[z].header + "\"],\n"
+                    else if (z < lastIndexOfContent)
+                        multiAppendingString += "\"" + this.props.ent[z].header + "\", "
+                    }
+            }
+        }
+
+        if (description_found > -1){
+            
+            multiAppendingString += "     description: ["
+            for (let z = 0; z < this.props.ent.length; z++){
+                if (this.props.ent[z].sesarTitle === "description"){
+                    if (z === description_found && (size_found < 0))
+                        multiAppendingString += "\"" + this.props.ent[z].header + "\"]\n"
+                    else if (z === description_found && (size_found > -1))
+                        multiAppendingString += " \"" + this.props.ent[z].header + "\"],\n"
+                    else if (z < lastIndexOfContent)
+                        multiAppendingString += "\"" + this.props.ent[z].header + "\", "
+                    }
+            }
+        }
+
+        if (size_found > -1){
+            
+            multiAppendingString += "     size: ["
+            for (let z = 0; z < this.props.ent.length; z++){
+                if (this.props.ent[z].sesarTitle === "size"){
+                    if (z === size_found)
+                        multiAppendingString += "\"" + this.props.ent[z].header + "\"]\n"
+                    else if (z < lastIndexOfContent)
+                        multiAppendingString += "\"" + this.props.ent[z].header + "\", "
+                    }
+            }
+        }
+
+            let appendingString = singlesAppendingString + multiAppendingString + "}\n"
 
         letMapString = letMapString.concat(appendingString)
         
