de11aca510e51900618d4117de3dbdf99cb325b5
hafey1 <niggebruggere@g.cofc.edu>
2020-10-29 18:57:51 -0400

    manual Template For Testing

--- src/__tests__/testRunner.js
+++ src/__tests__/testRunner.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
 //{ } imports non default component
 import { DropDown } from "../components/DropDown";
+//
+const fsLibrary = require('fs');
 
-
-describe('DropDown Component', () => {
-	it('DropDown.entWithContent(input) should return 1', () => {
+//
+function Joe() { 
+	
+	
+		//
+		let actualValues = []
+		//
 		const wrapper = shallow(<DropDown list={[]} />);
-		let result = wrapper.instance().entWithContent([{"value":""},{"value":"X"}]);
-		expect(result).toBe(1);
-	});
-});
+		
+		//
+		var resultof1 = wrapper.instance().entWithContent([{"value":""},{"value":"X"}]);
+		var resultof2 = wrapper.instance().entWithContent([{"value":""},{"value":""}]);
+		actualValues.push({"ID": 1, "actualResult": resultof1})
+		actualValues.push({"ID": 2, "actualResult": resultof2})
+		console.log(JSON.stringify(actualValues) + " this is result")
+		//
+		const returnValuesWrapped = { results: actualValues }
+		const converttoJSON = JSON.stringify(returnValuesWrapped, null, 4)
+		fsLibrary.writeFile('../../reports/actualResults.json', converttoJSON, (error) => {
+			
+			if (error) throw error;
+			
+			});
+		
+
+
+		//
+
 
 describe('DropDown Component', () => {
 	it('DropDown.entWithContent(input) should return 0', () => {
@@ -20,3 +42,6 @@ describe('DropDown Component', () => {
 	});
 });
 
+}
+Joe()
+console.log("log after Joe")
