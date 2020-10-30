4e5a1f01bcb870efa19921419cf995676c39e16f
hafey1 <niggebruggere@g.cofc.edu>
2020-10-30 15:14:00 -0400

    fixed bug found with tests

--- src/__tests__/testRunner.js
+++ src/__tests__/testRunner.js
@@ -1,47 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow } from 'enzyme'
 //{ } imports non default component
 import { DropDown } from "../components/DropDown";
-//
+
 const fsLibrary = require('fs');
 
-//
-function Joe() { 
-	
-	
-		//
-		let actualValues = []
-		//
-		const wrapper = shallow(<DropDown list={[]} />);
-		
-		//
-		var resultof1 = wrapper.instance().entWithContent([{"value":""},{"value":"X"}]);
-		var resultof2 = wrapper.instance().entWithContent([{"value":""},{"value":""}]);
-		actualValues.push({"ID": 1, "actualResult": resultof1})
-		actualValues.push({"ID": 2, "actualResult": resultof2})
-		console.log(JSON.stringify(actualValues) + " this is result")
-		//
-		const returnValuesWrapped = { results: actualValues }
-		const converttoJSON = JSON.stringify(returnValuesWrapped, null, 4)
-		fsLibrary.writeFile('../../reports/actualResults.json', converttoJSON, (error) => {
-			
-			if (error) throw error;
-			
-			});
-		
+let actualValues = []
+
+const wrapper = shallow(<DropDown list={[]} />);
+
+let resultOf1 = wrapper.instance().entWithContent([{"value":""},{"value":"X"}]);
+actualValues.push({ "ID": 1, "actualResult": resultOf1});
+
+let resultOf2 = wrapper.instance().entWithContent([{"value":""},{"value":""}]);
+actualValues.push({ "ID": 2, "actualResult": resultOf2});
+
+let resultOf3 = wrapper.instance().entWithContent(2);
+actualValues.push({ "ID": 3, "actualResult": resultOf3});
+
+let resultOf4 = wrapper.instance().entWithContent(3.14);
+actualValues.push({ "ID": 4, "actualResult": resultOf4});
+
+let resultOf5 = wrapper.instance().entWithContent("cat");
+actualValues.push({ "ID": 5, "actualResult": resultOf5});
 
 
-		//
-
-
-describe('DropDown Component', () => {
-	it('DropDown.entWithContent(input) should return 0', () => {
-		const wrapper = shallow(<DropDown list={[]} />);
-		let result = wrapper.instance().entWithContent([{"value":""},{"value":""}]);
-		expect(result).toBe(-1);
-	});
+const returnValuesWrapped = { results: actualValues }
+const converttoJSON = JSON.stringify(returnValuesWrapped, null, 4)
+fsLibrary.writeFile('../../reports/actualResults.json', converttoJSON, (error) => {
+	if (error) throw error;
 });
 
-}
-Joe()
-console.log("log after Joe")
+describe('dummy', () => {
+	it('0 to be 0', () => {
+		expect(0).toBe(0);});});
